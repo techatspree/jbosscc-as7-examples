3931c3e93c605425d19eadfd61deb8c7022cec16
Heinz Wilming <heinz.wilming@akquinet.de>
2012-06-15 11:14:32 +0200

    add callback method for stateful session bean

--- cluster-example/src/main/java/de/akquinet/jbosscc/cluster/ClusteredStatefulBean.java
+++ cluster-example/src/main/java/de/akquinet/jbosscc/cluster/ClusteredStatefulBean.java
@@ -2,12 +2,13 @@ package de.akquinet.jbosscc.cluster;
 
 import java.util.logging.Logger;
 
+import javax.ejb.PostActivate;
+import javax.ejb.PrePassivate;
 import javax.ejb.Stateful;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
 import org.jboss.ejb3.annotation.Clustered;
-
 
 @Stateful
 @SessionScoped
@@ -15,12 +16,23 @@ import org.jboss.ejb3.annotation.Clustered;
 @Named
 public class ClusteredStatefulBean {
 
-	private final static Logger LOG = Logger.getLogger(ClusteredStatefulBean.class.getName());
+	private final static Logger LOG = Logger
+			.getLogger(ClusteredStatefulBean.class.getName());
 
 	private int counter;
 
 	public int getCounterValue() {
 		LOG.info("invoke getCounter()");
 		return counter++;
+	}
+
+	@PrePassivate
+	public void passivate() {
+		LOG.info("passivate ejb component: " + this);
+	}
+
+	@PostActivate
+	public void activate() {
+		LOG.info("activate ejb component: " + this);
 	}
 }
