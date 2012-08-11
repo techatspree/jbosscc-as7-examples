672eb5d319d391bd6bb32c9ab022ce2e920930d0
Heinz Wilming <heinz.wilming@akquinet.de>
2012-08-11 17:22:55 +0200

    management client example

---
+++ dmr-example/src/main/java/de/akquinet/jbosscc/Main.java
@@ -0,0 +1,27 @@
+package de.akquinet.jbosscc;
+
+import java.net.InetAddress;
+
+import org.jboss.as.controller.client.ModelControllerClient;
+import org.jboss.dmr.ModelNode;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		ModelControllerClient client = ModelControllerClient.Factory.create(InetAddress.getByName("localhost"),9999);
+		
+		
+		ModelNode operation = new ModelNode();
+		operation.get("operation").set("read-resource-description");
+		operation.get("recursive").set(true);
+		operation.get("operations").set(true);
+		ModelNode address = operation.get("address");
+		address.add("subsystem", "web");
+		address.add("connector", "http");
+		
+		ModelNode returnVal = client.execute(operation);
+		System.out.println(returnVal.get("result").toString());
+		
+		
+	}
+}
