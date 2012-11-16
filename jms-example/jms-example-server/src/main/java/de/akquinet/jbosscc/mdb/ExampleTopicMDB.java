dd15f07536453694a4c1e92547f9bc06c2892d89
inspector-cochise <immanuel.sims@akquinet.de>
2012-11-16 17:06:03 +0100

    Bessere Ausgabe von MDBs und anpassen der Queue- bzw. Topicadresse. (Achtung es ist trotzdem ein gesonderter Entry ohne jboss/exported/ notwendig)

--- jms-example/jms-example-server/src/main/java/de/akquinet/jbosscc/mdb/ExampleTopicMDB.java
+++ jms-example/jms-example-server/src/main/java/de/akquinet/jbosscc/mdb/ExampleTopicMDB.java
@@ -2,21 +2,32 @@ package de.akquinet.jbosscc.mdb;
 
 import javax.ejb.ActivationConfigProperty;
 import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
+import javax.jms.TextMessage;
 
 @MessageDriven(activationConfig = {
 		@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Topic"),
-		@ActivationConfigProperty(propertyName = "destination", propertyValue = "topic/test")})
+		@ActivationConfigProperty(propertyName = "destination", propertyValue = "jms/topic/test")})
 public class ExampleTopicMDB implements MessageListener {
 
 	@Override
 	public void onMessage(Message message) {
-
-		System.out.println("----------------");
-		System.out.println("Received message");
-		System.out.println("----------------");
-
+		if (message instanceof TextMessage) {
+			try {
+				System.out.println("----------------");
+				System.out.println("Received: "
+						+ ((TextMessage) message).getText());
+				System.out.println("----------------");
+			} catch (JMSException e) {
+				e.printStackTrace();
+			}
+		} else {
+			System.out.println("----------------");
+			System.out.println("Received: " + message);
+			System.out.println("----------------");
+		}
 	}
 
 }
